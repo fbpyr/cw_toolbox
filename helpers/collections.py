7a8201a93cde6d599b1f5e838df77cb486073332
fbpyr <frederic.beaupere@erne.net>
2021-11-19 07:49:05 +0100

    add get_element_ids_spatially_sorted

--- helpers/collections.py
+++ helpers/collections.py
@@ -4,11 +4,12 @@ import element_controller   as ec
 import attribute_controller as ac
 
 from .param import get_element_type_info
+from .bbox  import get_bbox_from_elem_id
 
 
-def get_element_ids_by_type_name(preselected=None, quiet=None):
+def get_element_ids_by_type_name(preselected=None, quiet=None) -> defaultdict:
     """
-    Collects element ids of all ids or preselected id set
+    Collects element ids of all ids or preselected ids
     into dictionary grouped by element type name.
     :param preselected:
     :param quiet:
@@ -35,9 +36,9 @@ def get_element_ids_by_type_name(preselected=None, quiet=None):
     return elem_ids_by_type
 
 
-def get_element_names_by_id(preselected=None, quiet=None):
+def get_element_names_by_id(preselected=None, quiet=None) -> dict:
     """
-    Collects element names of all ids or preselected id set
+    Collects element names of all ids or preselected ids
     into dictionary grouped by element id.
     :param preselected:
     :param quiet:
@@ -58,9 +59,9 @@ def get_element_names_by_id(preselected=None, quiet=None):
     return elem_names_by_id
 
 
-def get_element_ids_by_name(preselected=None, quiet=None):
+def get_element_ids_by_name(preselected=None, quiet=None) -> defaultdict:
     """
-    Collects element ids of all ids or preselected id set
+    Collects element ids of all ids or preselected ids
     into dictionary grouped by element name.
     :param preselected:
     :param quiet:
@@ -81,4 +82,34 @@ def get_element_ids_by_name(preselected=None, quiet=None):
                 print(elem_id)
 
     return elem_ids_by_name
+
+
+def get_element_ids_spatially_sorted(preselected=None, quiet=None) -> dict:
+    """
+    Collects element ids of all ids or preselected ids
+    into dictionary of spatially sorted (by centroid y,x,z coordinate) element ids.
+    :param preselected:
+    :param quiet:
+    :return:
+    """
+    if not preselected:
+        preselected = ec.get_all_identifiable_element_ids()
+
+    elem_centroids = {}
+    for elem_id in preselected:
+        elem_bbox = get_bbox_from_elem_id(elem_id)
+        centroid = (
+            elem_bbox.centroid.x,
+            elem_bbox.centroid.y,
+            elem_bbox.centroid.z,
+        )
+        elem_centroids[centroid] = elem_id
+
+    elem_ids_sorted_by_centroid = {}
+    for i, (centroid, elem_id) in enumerate(sorted(elem_centroids.items())):
+        elem_ids_sorted_by_centroid[centroid] = elem_id
+        if not quiet:
+            print(f"{i:6} {elem_id} {centroid}")
+
+    return elem_ids_sorted_by_centroid
 
