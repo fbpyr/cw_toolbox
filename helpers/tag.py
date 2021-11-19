355de7c9a54c4747f9ddfea259e38e0eee9bb408
fbpyr <frederic.beaupere@erne.net>
2021-11-19 07:46:19 +0100

    add generic text label

--- helpers/tag.py
+++ helpers/tag.py
@@ -4,15 +4,27 @@ import geometry_controller      as gc
 
 def label_elem_with_id_text(elem_id, text_size=None):
     """
-    Adds 3d id text label to given element ids.
+    Adds 3d id text label to centroid of given element ids.
     :param elem_id:
     :param text_size:
     :return:
     """
-    ec.create_text_object(
-        str(elem_id),
+    return label_elem_with_text(elem_id, str(elem_id), text_size=None)
+
+
+def label_elem_with_text(elem_id, text, text_size=None):
+    """
+    Adds 3d text label with given text to centroid of given element ids.
+    :param elem_id:
+    :param text:
+    :param text_size:
+    :return:
+    """
+    return ec.create_text_object(
+        text,
         gc.get_center_of_gravity(elem_id),
         gc.get_local_x(),
         gc.get_local_y(),
         text_size or 250,
     )
+
