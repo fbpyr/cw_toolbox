2dfbdb96fbc1acc2f58bba59031d6d252b61e6ab
fbpyr <frederic.beaupere@erne.net>
2021-12-01 15:26:53 +0100

    move reset to repl

--- helpers/repl.py
+++ helpers/repl.py
@@ -1,4 +1,15 @@
 import sys
+import element_controller       as ec
+import visualization_controller as vc
+
+
+def reset():
+    """
+    shortcut to delete all elements in model
+    :return:
+    """
+    ec.delete_elements(ec.get_all_identifiable_element_ids())
+    vc.refresh()
 
 
 def rq():
