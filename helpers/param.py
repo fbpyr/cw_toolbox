b4809ddf16afe63c3790e0423fef6fce134e4d90
fbpyr <frederic.beaupere@erne.net>
2022-02-10 15:42:39 +0100

    add ifc type info

--- helpers/param.py
+++ helpers/param.py
@@ -1,8 +1,10 @@
 import json
 from pathlib import Path
 from functools import lru_cache
-import attribute_controller as ac
-import utility_controller   as uc
+import attribute_controller     as ac
+import utility_controller       as uc
+import bim_controller           as bc
+import visualization_controller as vc
 
 
 def get_element_info(elem_id: int) -> dict:
@@ -12,16 +14,24 @@ def get_element_info(elem_id: int) -> dict:
     :return:
     """
     element_info = {
-        "id"       : elem_id,
-        "name"     : ac.get_name(elem_id),
-        "comment"  : ac.get_comment(elem_id),
-        "group"    : ac.get_group(elem_id),
-        "subgroup" : ac.get_subgroup(elem_id),
+        "id"           : elem_id,
+        "name"         : ac.get_name(elem_id),
+        "comment"      : ac.get_comment(elem_id),
+        "group"        : ac.get_group(elem_id),
+        "subgroup"     : ac.get_subgroup(elem_id),
+        "material_name": ac.get_element_material_name(elem_id),
+        "color_index"  : vc.get_color(elem_id),
+        "building_name": bc.get_building(elem_id),
+        "storey_name"  : bc.get_storey(elem_id),
     }
     if get_element_type_info(elem_id):
         element_info["type_info"] = get_element_type_info(elem_id)[0]
     else:
         element_info["type_info"] = ""
+    if get_element_ifc_type_info(elem_id):
+        element_info["ifc_type_info"] = get_element_ifc_type_info(elem_id)[0]
+    else:
+        element_info["ifc_type_info"] = ""
     return element_info
 
 
@@ -34,6 +44,25 @@ def get_element_type_info(elem_id: int) -> list:
     type_info = []
     element_type = ac.get_element_type(elem_id)
     for attr in dir(element_type):
+        if attr.startswith("is_"):
+            attr_value = getattr(element_type, attr)()
+            #print(attr, attr_value)
+            if attr_value:
+                type_info.append(attr[3:])
+    return type_info
+
+
+def get_element_ifc_type_info(elem_id: int) -> list:
+    """
+    Iterates over element ifc type attributes to retrieve information
+    :param elem_id:
+    :return:
+    """
+    type_info = []
+    element_type = bc.get_ifc2x3_element_type(elem_id)
+    for attr in dir(element_type):
+        if attr == "is_none":
+            continue
         if attr.startswith("is_"):
             attr_value = getattr(element_type, attr)()
             #print(attr, attr_value)
