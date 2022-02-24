384aaaf44cf5db2b3ff6f18b31cb2ae88af200c1
fbpyr <frederic.beaupere@erne.net>
2022-02-24 16:35:04 +0100

    add repl interrupt

--- helpers/repl.py
+++ helpers/repl.py
@@ -1,15 +1,42 @@
 import sys
+from tkinter import Tk, Label
 import element_controller       as ec
 import visualization_controller as vc
 
 
 def reset():
     """
-    shortcut to delete all elements in model
+    shortcut to delete all elements in model - DANGER!!
     :return:
     """
     ec.delete_elements(ec.get_all_identifiable_element_ids())
     vc.refresh()
+
+
+def interrupt_repl():
+    """
+    Blocks repl session with tkinter helper to access cadwork application.
+    Resumes the repl session on closing the tkinter helper.
+    :return:
+    """
+    window = Tk()
+    window.geometry("400x100")
+    window.title("Close this window resume repl session")
+    window.config(background="#666666")
+    cw_style = {
+        "font": ("Arial", 10),
+        "bg": "#666666",
+        "fg": "#ffffff",
+    }
+    text = "This blocks the current repl session.\n"\
+           "Close this window resume repl session."
+    label = Label(
+        master=window,
+        text=text,
+        **cw_style
+    )
+    label.pack(padx=5, pady=5, anchor="w")
+    window.mainloop()
 
 
 def rq():
